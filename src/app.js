fe0d377e2d0ad77e70280e8aef8223073ff47936
Bharath <104580107+BharathkumarRangaraj@users.noreply.github.com>
2024-09-22 12:50:56 +0530

    intial setup express and node setup

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express=require('express');
+const app=express();
+app.use("/test",(req,res)=>{
+    res.send('its test')
+});
+app.use((req,res)=>{
+    res.send('its home')
+})
+
+app.listen(7777,()=>console.log("port running in 7777"));
