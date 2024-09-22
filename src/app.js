513b62b162838d75bfff6dc76d4e6f8b73440a6c
Bharath <104580107+BharathkumarRangaraj@users.noreply.github.com>
2024-09-22 18:35:47 +0530

    exploriingpostmanapi

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
-const express=require('express');
-const app=express();
-app.use("/test",(req,res)=>{
-    res.send('its test')
+const express = require("express");
+const app = express();
+
+//order does matter in routing
+app.get("/user", (req, res) => {
+  res.send("posted");
 });
-app.use((req,res)=>{
-    res.send('its home')
-})
-app.use("/hey",(req,res)=>{
-    res.send('its hey')
-})
-app.listen(7777,()=>console.log("port running in 7777"));
+
+app.delete("/user", (req, res) => {
+  res.send("deleted user");
+});
+app.patch("/user", (req, res) => {
+  res.send("patched user");
+});
+app.listen(7777, () => console.log("port running in 7777"));
