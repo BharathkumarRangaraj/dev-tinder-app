576b01c942bb08142ca53171a977db351a980bc7
Bharath <104580107+BharathkumarRangaraj@users.noreply.github.com>
2024-09-22 18:57:49 +0530

    play with routing ,query,params

--- src/app.js
+++ src/app.js
@@ -2,14 +2,14 @@ const express = require("express");
 const app = express();
 
 //order does matter in routing
-app.get("/user", (req, res) => {
-  res.send("posted");
+app.get("/abc/:pass/:place", (req, res) => {
+    console.log(req.query);
+    console.log(req.params);
+    
+    
+    
+  res.send({name:'raja',age:'34'});
 });
 
-app.delete("/user", (req, res) => {
-  res.send("deleted user");
-});
-app.patch("/user", (req, res) => {
-  res.send("patched user");
-});
+
 app.listen(7777, () => console.log("port running in 7777"));
