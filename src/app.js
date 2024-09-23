0aad2a94054db3593753285a60784467c11580a1
Bharath <104580107+BharathkumarRangaraj@users.noreply.github.com>
2024-09-23 12:07:01 +0530

    multiple nested route handlers

--- src/app.js
+++ src/app.js
@@ -2,14 +2,26 @@ const express = require("express");
 const app = express();
 
 //order does matter in routing
-app.get("/abc/:pass/:place", (req, res) => {
-    console.log(req.query);
-    console.log(req.params);
-    
-    
-    
-  res.send({name:'raja',age:'34'});
-});
+app.get(
+  "/abc",
+  (req, res, next) => {
+    console.log("hey its one");
+    next();
+    // res.send({ name: "raja", age: "34" });
 
+  },
+  (req,res,next) => {
+    console.log("hey its 2");
+
+    // res.send("hey its 2");
+    next()
+  },
+    (req,res)=>{
+console.log('hey its 3');
+res.send('sending 3rd route paras')
+
+    }
+  
+);
 
 app.listen(7777, () => console.log("port running in 7777"));
