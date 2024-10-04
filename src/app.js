5ea0861ce1af8988804ea0cef9bc9406623bdbae
Bharath <104580107+BharathkumarRangaraj@users.noreply.github.com>
2024-10-04 12:12:51 +0530

    ep-07-get user and fetch data in postman

--- src/app.js
+++ src/app.js
@@ -3,16 +3,44 @@ const app = express();
 const { connectDB } = require("./config/database");
 const user = require("./modal/user");
 
-app.use(express.json())
+app.use(express.json());
 
 //creating post API for signup.
-
 app.post("/signup", async (req, res) => {
   const userdata = new user(req.body);
 
   try {
     await userdata.save();
-    res.send('user added successfully')
+    res.send("user added successfully");
+  } catch (error) {
+    console.log("error received", error.message);
+  }
+});
+
+//get user by emailid
+app.get("/user", async (req, res) => {
+  const oneuser = req.body.email;
+  try {
+    const userr = await user.findOne({ mail: oneuser });
+    if (!user) {
+      console.log("error received", error.message);
+    } else {
+      console.log(userr);
+
+      res.send(userr);
+    }
+  } catch (error) {
+    console.log("error received", error.message);
+  }
+});
+
+//get all the users //feed api
+
+app.get("/fetch", async (req, res) => {
+
+  try {
+    const userr = await user.find({ });
+    res.send(userr)
   } catch (error) {
     console.log("error received", error.message);
   }
