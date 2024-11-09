4af99ef9053c181ea2bb0734f2994d624fdfda6d
Bharath <104580107+BharathkumarRangaraj@users.noreply.github.com>
2024-11-09 12:09:41 +0530

    PRE-checking the data before saving

--- src/modal/connectionRequest.js
+++ src/modal/connectionRequest.js
@@ -19,5 +19,10 @@ const connectionRequestSchema=new mongoose.Schema({
     }
 });
 
-
+connectionRequestSchema.pre("save",function(next){
+    const connectionRequest=this;
+    if(connectionRequest.fromUserId.equals(connectionRequest.toUserId)){
+        throw new Error('cannot send request to yourself');
+    }
+})
 module.exports=mongoose.model('connectionRequest',connectionRequestSchema);
